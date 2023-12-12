269050628cb81f544621313a5bd33f38d619ce5a
Oscar Montecinos <oscar.montecinos.guerrero@gmail.com>
2023-12-12 19:18:32 -0300

    implementacion quicksort

--- proyectoUnidad3.cpp
+++ proyectoUnidad3.cpp
@@ -31,17 +31,45 @@ void SelectionSort(vector<int>& arr) {
         }
     }
 }
-double getResultFromAlg(vector<int>& arr,int option) {
+
+void quickSort(vector<int>& arr, int low, int high){
+    if(low < high){
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for(int j = low; j <= high - 1; j++){
+            if(arr[j] < pivot){
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        int pivotActual = i + 1;
+        quickSort(arr,low,pivotActual - 1);
+        quickSort(arr,pivotActual + 1,high);
+    }
+}
+
+
+double getResultFromAlg(vector<int>& arr) {
     time_t start, end;
     double time_taken;
     time(&start);
     ios_base::sync_with_stdio(false);
+
+                        //cambiar esta funcion hace el tiempo del algoritmo usado.
     SelectionSort(arr);
+    //quickSort(arr,0,arr.size()- 1);
+
     time(&end);
     time_taken = double(end - start);
     return time_taken;
 }
 int main(int argc, char* argv[]) {
+    const int amount = 1000;
+    vector<int>arr, arrSorted,arrReverse;
+    int random_value;
+
     cout << "Generando set de datos: " << endl;
     for (int i = 0; i < amount ; ++i) {
         arrSorted.push_back(i+1);
@@ -56,7 +84,7 @@ int main(int argc, char* argv[]) {
         }
     }
     unordered_map<string, double> results;
-    results["SeleciontSort"] = getResultFromAlg(arr);
+    results["SelectionSort"] = getResultFromAlg(arr);
     vector<int> arr1,arr2;
     arr1.assign(arr.begin(), arr.end());
     arr2.assign(arr.begin(), arr.end());
